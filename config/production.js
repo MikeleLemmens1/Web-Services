1351b845abfe96229da81909fa94da3ef0d19b24
Mikele Lemmens <mikele.lemmens@student.hogent.be>
2023-12-22 11:54:42 +0100

    "Database properties niet meegegeven"

--- config/production.js
+++ config/production.js
@@ -3,11 +3,14 @@ module.exports = {
     level:"info",
     disabled:false
   },
-  host:{
-    port:9000
-  },
   cors: { // 👈 1
     origins: ['http://localhost:5173'], // 👈 2
     maxAge: 3 * 60 * 60, // 👈 3
+  },
+  database: {
+    client: 'mysql2',
+    host: 'localhost',
+    port: 3306,
+    name: 'gezinsplanner',
   },
 }
